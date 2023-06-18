6fb05f94ac911ec981e717a02e86ee22484c7cea
allefgoncalves <allefgoncalves@gmail.com>
2023-06-18 07:07:58 -0300

    renderização dos quizzes

--- index.js
+++ index.js
@@ -4,8 +4,41 @@ const objquizz={
 
 }
 
-const promise = axios.post('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes', objquizz);
-promise.then(console.log('deu certo'));
+let todoOsQuizzes =[];
+
+function renderrizarQuizzes(){
+    const elementoUl = document.querySelector('.todos-os-quizzes');
+    elementoUl.innerHTML = '';
+
+    const arrayElementos = todoOsQuizzes.map( function(quizz){
+        console.log(quizz.title);
+        return `
+        <li>
+            <button style="background-image:url(${quizz.image});" class="habilitado">
+                <h2>${quizz.title}</h2>
+            </button>
+        </li> 
+        `;
+    });
+
+    arrayElementos.forEach( function (li){
+        elementoUl.innerHTML += li;
+    });
+}
+
+function sucessoBuscarQuizzes(resposta){
+    console.log(resposta.data);
+    todoOsQuizzes = resposta.data;
+    renderrizarQuizzes();
+}
+
+function erroBuscarQuizzes(erro){
+    console.log(erro);
+}
+
+const promise = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
+//const promise = axios.post('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes', objquizz);
+promise.then(sucessoBuscarQuizzes);
 promise.catch(console.log('deu errado'));
 
 console.log(objquizz);
