a6da064bea3c3a048f8bfdd7fd7f04e1173e0a8e
allefgoncalves <allefgoncalves@gmail.com>
2023-06-18 10:33:38 -0300

    layout das perguntas

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const objquizz={
 }
 
 let todoOsQuizzes =[];
+let meusQuizzes=[];
+
+//OBS: tem que ajustar a função
+function renderrizarMeusQuizzes(){
+    const elementoUl = document.querySelector('.todos-os-quizzes');
+    elementoUl.innerHTML = '';
+
+    const arrayElementos = todoOsQuizzes.map( function(quizz){
+        console.log(quizz.title);
+        return `
+        <li data-test="my-quiz">
+            <button 
+            style="background-image:url(${quizz.image});" 
+            class="habilitado"
+            onclick="jogarQuizz()">
+                <h2>${quizz.title}</h2>
+            </button>
+        </li> 
+        `;
+    });
+
+    arrayElementos.forEach( function (li){
+        elementoUl.innerHTML += li;
+    });
+}
 
 function renderrizarQuizzes(){
     const elementoUl = document.querySelector('.todos-os-quizzes');
@@ -13,8 +38,10 @@ function renderrizarQuizzes(){
     const arrayElementos = todoOsQuizzes.map( function(quizz){
         console.log(quizz.title);
         return `
-        <li>
-            <button style="background-image:url(${quizz.image});" class="habilitado">
+        <li data-test="others-quiz">
+            <button 
+            style="background-image:url(${quizz.image});"
+            onclick="jogarQuizz()">
                 <h2>${quizz.title}</h2>
             </button>
         </li> 
@@ -37,8 +64,48 @@ function erroBuscarQuizzes(erro){
 }
 
 const promise = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
-//const promise = axios.post('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes', objquizz);
+
 promise.then(sucessoBuscarQuizzes);
+
+//obs: está execultando o catch - verificar
 promise.catch(console.log('deu errado'));
 
-console.log(objquizz);
+//função que verifica a resposta correta da alternativa
+function resposta(){
+    //faz a verificação do ditetorio:
+    //questions.(variavel).answers.(variavel).isCorrectAnswer
+    console.log("teste");
+}
+
+//função que vai para as perguntas
+function jogarQuizz(quizzselecionado){
+/*  desabillitar:
+    .start
+    habilitar:
+    
+*/
+
+console.log('clicou'); //teste
+
+}
+
+
+
+
+//função que cria os quizzes 
+function createQuizz(){
+/*  desabillitar:
+
+    habilitar:
+    
+*/
+
+/*quando add quizze(s) 
+    desabillitar:
+
+    habilitar:
+
+*/ 
+
+console.log(criarquizz);
+}
